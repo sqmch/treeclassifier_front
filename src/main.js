6113bc7bd67871ed468e3af76556ba3d620ef350
sqaku <sqmchh@gmail.com>
2019-04-10 11:55:24 +0100

    main.js tweak

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueMaterial from "vue-material";
-import "vue-material/dist/vue-material.min.css";
+import("vue-material/dist/vue-material.min.css")
 import VueVisible from "vue-visible";
 
 Vue.config.productionTip = false;
